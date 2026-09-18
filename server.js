ff182e01f60682ab923bee18bba1f36ed96f0725
Bhuvaneshwer A <bhuvaneshwera@gmail.com>
2026-09-18 15:24:13 +0100

    shipyard test 1

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 10000;
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const WORLD = {
     width: 2400,
